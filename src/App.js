23bc4603514150745a87804136ab97be269f02c0
Aaron <ajonaaronajon@yahoo.com>
2022-07-14 21:30:38 -0500

    add useGeItem custom component for generating random image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+const useGeItem = ()=>{
+  const [item, setItem] = useState([]);
 
-  const fetchGrandExchangeData = async ()=>{
+  const fetchItem = async ()=>{
     const url = "https://prices.runescape.wiki/api/v1/osrs/mapping";
     const response = await fetch(url, {mode: "cors"});
+    
     const data = await response.json();
-    console.log(data[0]);
+    
+    const ids = data.map(item => item.id);
+    const itemId = ids[Math.floor(Math.random() * ids.length)]
+    //console.log(data.filter((item)=>item.id == itemId));
+    const itemSrc = `https://secure.runescape.com/m=itemdb_oldschool/obj_sprite.gif?id=${itemId}`
+    setItem(itemSrc);
   };
 
-  fetchGrandExchangeData();
+  useEffect = (()=>{
+    fetchItem();
+  }, []);  
+
+  return {item, fetchItem}
+}
+
+
+function App() {
+//endpoint for accessing ge item images 
+//https://secure.runescape.com/m=itemdb_oldschool/obj_sprite.gif?id=3200 <---adamant halberd
+
+ const {item, fetchItem} = useGeItem([]);
+  
+
+  fetchItem();
+
 
   return (
     <div className="App">
-
+        <img src={item} height="100px" width="100px"/>
     </div>
   );
 }
