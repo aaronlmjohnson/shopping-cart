43966e681277d7861f780edf6a004b5fca70d179
Aaron <ajonaaronajon@yahoo.com>
2022-07-20 18:09:48 -0500

    Add: GeItems.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import GeItems from './GeItems';
 import useFilterGeItems from './component/usefilterGeItems';
 import useLoadGeItems from './component/useLoadGeItems';
 
+
+// for(let i = 0; i < 121; i+=12){
+//   splitByTwelve.push(numbers.slice(i, numbers[i+10-1] ? i+10 : numbers.length -1]));
+// }
 function App() {
 
   const {items, fetchItems} = useLoadGeItems([]);
