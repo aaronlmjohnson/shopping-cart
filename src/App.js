f6f6f358c67cc11f45735d4d59ec039bd7d1830a
Aaron <ajonaaronajon@yahoo.com>
2022-07-19 20:08:49 -0500

    Add: useLoadGeItems component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Searchbar from './Searchbar';
 import GeItems from './GeItems';
 import useGeItems from './useGeItems';
+import useLoadGeItems from './component/useLoadGeItems';
 
 function App() {
 
- const {items, fetchItems} = useGeItems([]);
+ //const {items, fetchItems} = useGeItems([]);
+  const {items, fetchItems} = useLoadGeItems([]);
 
   return (
     <div className="App">
-      <Searchbar fetchItems={fetchItems}/>
-      <GeItems items={items}/>
+      {/* <Searchbar fetchItems={fetchItems}/>
+      <GeItems items={items}/> */}
     </div>
   );
 }
---
+++ src/component/useLoadGeItems.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react';
+
+const useLoadGeItems = ()=>{
+    const [items, setItems] = useState([]);
+  
+    const fetchItems = async (name)=>{
+      const url = `https://prices.runescape.wiki/api/v1/osrs/mapping`;
+      //wiki ge complete item list https://prices.runescape.wiki/api/v1/osrs/mapping
+      //wiki search by specific item with id https://prices.runescape.wiki/api/v1/osrs/latest?id=10344 <- 3rd age amulet
+      
+      const response = await fetch(url, {mode: 'cors'});
+      
+      const data = await response.json();
+      console.log(data);
+      setItems(data);
+    };
+  
+    useEffect(()=>{
+      fetchItems();
+    }, []);  
+  
+    return {items, fetchItems}
+}
+
+export default useLoadGeItems;
