75215f2e34a95d7fd908fef536cc148af4045924
Aaron <ajonaaronajon@yahoo.com>
2022-07-21 18:22:55 -0500

    Add: useLoadGePrices to fetch GE prices from RS Wiki API.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Searchbar from './Searchbar';
 import GeItems from './GeItems';
 import useFilterGeItems from './component/usefilterGeItems';
 import useLoadGeItems from './component/useLoadGeItems';
+import useLoadGePrices from './component/useLoadGePrices';
 
 
 // for(let i = 0; i < 121; i+=12){
-//   splitByTwelve.push(numbers.slice(i, numbers[i+10-1] ? i+10 : numbers.length -1]));
+//   splitByTwelve.push(numbers.slice(i, numbers[i+10-1] ? i+10 : numbers.length -1])); <-- for seperating into pages with twelve items each
 // }
 function App() {
 
   const {items, fetchItems} = useLoadGeItems([]);
+  const {prices, fetchPrices} = useLoadGePrices([]);
   const {filteredItems, filterItemsByName} = useFilterGeItems(items);
 
   return (
---
+++ src/component/useLoadGePrices.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react';
+
+const useLoadGePrices = ()=>{
+    const [prices, setPrices] = useState([]);
+  
+    const fetchPrices = async (name)=>{
+      const url = `https://prices.runescape.wiki/api/v1/osrs/latest?`;
+      //wiki ge complete item list https://prices.runescape.wiki/api/v1/osrs/mapping
+      //wiki search by specific item with id https://prices.runescape.wiki/api/v1/osrs/latest?id=10344 <- 3rd age amulet
+      
+      const response = await fetch(url, {mode: 'cors'});
+      
+      const data = await response.json();
+      console.log(data.data[3200]);
+      setPrices(data.data);
+    };
+  
+    useEffect(()=>{
+      fetchPrices();
+      console.log("Prices API loaded");
+    }, []);  
+  
+    return {prices, fetchPrices}
+}
+
+export default useLoadGePrices;
