aade3034e33691563360dc29524a56df69d62622
Aaron <ajonaaronajon@yahoo.com>
2022-07-22 09:25:05 -0500

    Add: styling to ItemCard.css

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -8,9 +8,9 @@ const ItemCard = (props)=>{
             <h1 className="item-name">{name}</h1>
             <img className="item-icon" src={`https://oldschool.runescape.wiki/images/${icon.replaceAll(' ', '_')}`} alt="Grand Exchange item" /> 
             <p className="item-examine">{examine}</p>
-            <div> 
-                <img src="https://oldschool.runescape.wiki/images/Coins_250.png?c2755" alt="Coins" />
-                <p className="price">{price.low}</p>
+            <div className="price-div"> 
+                <img src="https://oldschool.runescape.wiki/images/Coins_5.png?4afbb" alt="Coins" />
+                <p className="price">{price.low.toLocaleString('en-US')}</p>
             </div>
             
         </li>
