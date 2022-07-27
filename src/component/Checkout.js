88dba87f304320f822923c9b5e1fb54a322102c1
Aaron <ajonaaronajon@yahoo.com>
2022-07-27 17:08:31 -0500

    Added: styling to Checkout items

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -1,17 +1,19 @@
+import './Checkout.css';
 const Checkout = (props)=>{
     const { cart, removeFromCart } = props;
 
     return (
         <div id="checkout">
+            <h1 id="shopping-cart-title">Shopping Cart</h1>
             {Object.keys(cart).map((key)=>{
-                return (<div className="cart-item" key={`c${cart[key].id}`}>
-                        <img src={cart[key].img} alt={cart[key].name}/>
-                        <h1>{cart[key].name}</h1>
-                        <p>{cart[key].examine}</p>
-                        <p>Qty: {cart[key].quantity}</p>
-                        <p>{(cart[key].price * cart[key].quantity).toLocaleString('en-US')} gold</p>
-                        <button onClick={(e)=>removeFromCart(e, key)}>delete</button>
-                </div>);
+                return (<ul className="cart-item" key={`c${cart[key].id}`}>
+                        <li><img src={cart[key].img} alt={cart[key].name}/></li>
+                        <li><h1>{cart[key].name}</h1></li>
+                        <li className="cart-item-examine"><p>{cart[key].examine}</p></li>
+                        <li><p>Qty: {cart[key].quantity}</p></li>
+                        <li><p>{(cart[key].price * cart[key].quantity).toLocaleString('en-US')} gold</p></li>
+                        <li><button onClick={(e)=>removeFromCart(e, key)}>delete</button></li>
+                </ul>);
             })}
         </div>
     )
